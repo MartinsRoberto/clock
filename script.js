58125d30acb89eb76bc74ab1b19873f6d46586e4
MartinsRoberto <robertomartinsramm@gmail.com>
2022-12-10 23:48:51 -0300

    stopwatch reset resume stop

--- script.js
+++ script.js
@@ -7,23 +7,57 @@ let milliNum = 0
 let secNum = 0
 let minNum = 0
 let INTERVAL
+let resumeValue = true
 
 const start = document.querySelector('.start')
 const lap = document.querySelector('.lap')
 const resume = document.querySelector('.resume')
 const reset = document.querySelector('.reset')
-const stop = document.querySelector('.stop')
+const pause = document.querySelector('.pause')
 
 start.addEventListener('click', (e) => {
-    startStopwatch()
+  startStopwatch()
 
-    start.style.display = 'none'
-    stop.style.display = 'inline-block'
+  start.style.display = 'none'
+  pause.style.display = 'inline-block'
 })
 
-stop.addEventListener('click', () => {
+pause.addEventListener('click', () => {
+  pauseStopwatch()
 
+  resume.style.display = 'inline-block'
+  reset.style.display = 'inline-block'
+  pause.style.display = 'none'
+  start.style.display = 'none'
+  lap.style.display = 'none'
 })
+
+resume.addEventListener('click', () => {
+
+  pause.style.display = 'inline-block'
+  lap.style.display = 'inline-block'
+  resume.style.display = 'none'
+  reset.style.display = 'none'
+  start.style.display = 'none'
+
+  resumeValue = true
+})
+
+reset.addEventListener('click', () => {
+  resetStopwatch()
+
+  start.style.display = 'inline-block'
+  lap.style.display = 'inline-block'
+  resume.style.display = 'none'
+  reset.style.display = 'none'
+  pause.style.display = 'none'
+})
+
+// FUNCTIONS
+// FUNCTIONS
+// FUNCTIONS
+// FUNCTIONS
+// FUNCTIONS
 
 function startStopwatch() {
   clearInterval(INTERVAL)
@@ -32,21 +66,40 @@ function startStopwatch() {
   }, 10)
 }
 
-function milliStopwatch() {
-  milliNum++
-  if (milliNum < 10) {
-    milli.innerHTML = '0' + milliNum
-  } else {
-    milli.innerHTML = milliNum
-  }
+function pauseStopwatch() {
+  resumeValue = false
+}
 
-  if (milliNum == 99) {
-    milliNum = 0
-    secStopwatch()
+function resetStopwatch() {
+  clearInterval(INTERVAL)
+  milliNum = 0
+  secNum = 0
+  minNum = 0
+
+  min.innerText = '00' 
+  sec.innerText = '00' 
+  milli.innerText = '00' 
+
+  resumeValue = true
+}
+
+function milliStopwatch() {
+  if (resumeValue) {
+    milliNum++
+    if (milliNum < 10) {
+      milli.innerHTML = '0' + milliNum
+    } else {
+      milli.innerHTML = milliNum
+    }
+
+    if (milliNum == 99) {
+      milliNum = 0
+      secStopwatch()
+    }
   }
 }
 
-function secStopwatch(){
+function secStopwatch() {
   secNum++
   if (secNum < 10) {
     sec.innerHTML = '0' + secNum
@@ -60,7 +113,7 @@ function secStopwatch(){
   }
 }
 
-function minStopwatch(){
+function minStopwatch() {
   minNum++
   if (minNum < 10) {
     min.innerHTML = '0' + minNum
